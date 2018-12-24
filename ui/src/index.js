74c911f2ab030040c6691507572fc18b443bc261
Chris Frantz <frantzyy@yahoo.com>
2018-12-24 19:24:25 +0000

    added react client and basic connection to graphql

--- ui/src/index.js
+++ ui/src/index.js
@@ -1,10 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import ApolloClient from "apollo-boost";
+import gql from "graphql-tag";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql"
+});
+
+client
+  .query({
+    query: gql`
+      {
+        launch(id: 5) {
+          id
+          site
+          mission {
+            name
+          }
+          rocket {
+            id
+            name
+          }
+        }
+      }
+    `
+  })
+  .then(result => console.log(result));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
