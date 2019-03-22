cbb927dcecbeb6c623c8c73887e0e8671f861dbe
Chris Frantz <frantzyy@yahoo.com>
2019-03-21 21:39:21 -0400

    resolved issue launch list returning in graphiQlgit status

--- api/src/resolvers.js
+++ api/src/resolvers.js
@@ -1,23 +1,13 @@
 module.exports = {
   Query: {
-    launches: async (_, { pageSize = 20, after }, { dataSources }) => {
+    launches: async (_, __, { dataSources }) => {
       const allLaunches = await dataSources.spacexApi.getAllLaunches();
-      // we want these in reverse chronological order
       console.log("allLaunches: ", allLaunches);
       allLaunches.reverse();
 
-      //TODO: paginateResults
-      // const launches = paginateResults({
-      //   after,
-      //   pageSize,
-      //   results: allLaunches
-      // });
-
-      return {
-        // launches
-        allLaunches
-      };
+      return allLaunches;
     },
+
     launch: (_, { id }, { dataSources }) =>
       dataSources.spacexApi.getLaunchById({ launchId: id })
     // me: async (_, __, { dataSources }) => dataSources.userAPI.findOrCreateUser()
